4f2d5b36d861dcddf1fdbda7717fdd168f8cac9f
iu2019 <57230918+iu2019@users.noreply.github.com>
2020-05-04 23:18:01 +0100

    initial

---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container">
+    <h1>Hello</h1>
+    <input type="text" v-model="text" />
+    <p>{{ text }}</p>
+  </div>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue';
+
+export default {
+  data() {
+    return {
+      text: '',
+    };
+  },
+};
+</script>
+
+<style>
+input {
+  display: block;
+}
+</style>
